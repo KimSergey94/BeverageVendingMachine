3c8c486412dbe51568db0ebab344fdf2b37020b1
KimSergey94 <sksss060213@gmail.com>
2023-06-24 21:54:56 +0600

    Add-Migration CoinDepositedQuantityAndOperationStorageItem

---
+++ BeverageVendingMachine.Infrastructure/Migrations/20230624155339_CoinDepositedQuantityAndOperationStorageItem.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeverageVendingMachine.Infrastructure.Migrations
+{
+    public partial class CoinDepositedQuantityAndOperationStorageItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StorageItemId",
+                table: "CoinOperations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CoinOperations_StorageItemId",
+                table: "CoinOperations",
+                column: "StorageItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CoinOperations_StorageItems_StorageItemId",
+                table: "CoinOperations",
+                column: "StorageItemId",
+                principalTable: "StorageItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CoinOperations_StorageItems_StorageItemId",
+                table: "CoinOperations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CoinOperations_StorageItemId",
+                table: "CoinOperations");
+
+            migrationBuilder.DropColumn(
+                name: "StorageItemId",
+                table: "CoinOperations");
+        }
+    }
+}
